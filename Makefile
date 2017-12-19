0b900d0607bcee48e7ade41ed30c07099415f152
Izaak van Dongen <izaak.van.dongen@gmail.com>
2017-12-19 10:30:33 +0000

    added makefile and some more data

---
+++ Makefile
@@ -0,0 +1,2 @@
+stats.md: src/datasets file_analysis.zsh
+	./file_analysis.zsh > stats.md
--- src/file_analysis.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/zsh
-
-for i in mvd mtn_dew libertarian early half_mountain unethical joker stingy woody twoface; do
-    echo -n "$i | " | sed "s/_/\\\\_/g";
-    cat datasets/$i/* | pypy stats.py --file - --quiet | sed "s/,/ | /g";
-done;
